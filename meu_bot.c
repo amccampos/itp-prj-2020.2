35675b4931d8b199d1f3457de7bf25172a6a72ac
Isaque Barbosa <>
2021-04-13 19:28:01 +0000

    bot

--- meu_bot.c
+++ meu_bot.c
@@ -5,12 +5,15 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /** Tamanho máximo de uma linha da entrada-padrão */
 #define MAX_LINE 200
 
 /** Tamanho máximo do identificador do bot */
 #define ID_SIZE 20
+
+int indexCopas = 0, indexOuro = 0, indexPaus = 0,indexEspadas = 0;
 
 typedef struct cartas{
   int numero;
@@ -46,6 +49,125 @@ void bubblesortNaipes(Carta *mao, int index)
     }
 }
 
+char* puxar(){
+  return "GET_STOCK";
+}
+
+void valorCarta(char* carta, Carta* mao){
+  char valor;
+  char lixoBuffer;
+  char naipeAtual[5];
+
+  sscanf(carta, "%c", &valor);
+
+  if (valor == '1')
+    {
+      //Recebe o valor (1), um lixoBuffer (0), e o naipeAtual
+      sscanf(carta, "%c %c %s", &valor, &lixoBuffer, naipeAtual);
+    }
+    else{
+       //Recebe somente o valor e o naipeAtual
+       sscanf(carta, "%c %s", &valor, naipeAtual);
+    }
+
+     //Colocando o valor recebido
+    if(valor == '1')
+    {
+      mao->numero = 10;
+    }
+    else if (valor == 'A')
+    {
+     mao->numero = 1;
+    }
+    else if(valor == 'J')
+    {
+     mao->numero = 11;
+    }
+    else if(valor == 'Q')
+    {
+     mao->numero = 12;
+    }
+    else if(valor == 'K')
+    {
+     mao->numero = 13;
+    }
+    else{
+     mao->numero = valor - '0';
+    }
+
+    strcpy(mao->naipe, naipeAtual);
+}
+
+void tratamentoNaipe(Carta* mao, Carta* maoCopas, Carta* maoOuro, Carta* maoEspadas, Carta* maoPaus)
+{
+    if((strcmp(mao->naipe, "♥") == 0))
+    {
+      strcpy(maoCopas[indexCopas].naipe, mao->naipe);
+      maoCopas[indexCopas].numero = mao->numero;
+      indexCopas++;
+      // maoCopas = realloc(maoCopas, sizeof(Carta) * (1 + 1));
+    }
+    if((strcmp(mao->naipe, "♦") == 0))
+    {
+      strcpy(maoOuro[indexOuro].naipe, mao->naipe);
+      maoOuro[indexOuro].numero = mao->numero;
+      indexOuro++;
+      // maoOuro = realloc(maoOuro, sizeof(Carta) * (indexOuro + 1));
+    }
+    if((strcmp(mao->naipe, "♠") == 0))
+    {
+      strcpy(maoEspadas[indexEspadas].naipe, mao->naipe);
+      maoEspadas[indexEspadas].numero = mao->numero;
+      indexEspadas++;
+      // maoEspadas = realloc(maoEspadas, sizeof(Carta) * (indexEspadas + 1));
+    }
+    if((strcmp(mao->naipe, "♣") == 0))
+    {
+      strcpy(maoPaus[indexPaus].naipe, mao->naipe);
+      maoPaus[indexPaus].numero = mao->numero;
+      indexPaus++;
+      // maoPaus = realloc(maoPaus, sizeof(Carta) * (indexPaus + 1));
+    }
+}
+
+void printMaoNaipes(Carta* mao, Carta* maoCopas, Carta* maoOuro, Carta* maoEspadas, Carta* maoPaus, FILE* saida)
+{
+  int i = 0;
+    //Print Mão Copas
+    fprintf(saida, "Mao copas: [ ");
+    for (i = 0; i < indexCopas; i++){
+    fprintf(saida, "%d%s ", maoCopas[i].numero, maoCopas[i].naipe);
+    }
+    fprintf(saida, "] index: %d\n", indexCopas);
+
+
+    //Print Mão Ouro
+    fprintf(saida, "Mao ouro: [ ");
+    for (i = 0; i < indexOuro; i++)
+    {
+    fprintf(saida, "%d%s ", maoOuro[i].numero, maoOuro[i].naipe);
+    }
+    fprintf(saida, "] index: %d\n", indexOuro);
+
+
+    //Print Mão Espadas
+    fprintf(saida, "Mao espadas: [ ");
+    for(i = 0; i < indexEspadas; i++)
+    {
+    fprintf(saida, "%d%s ", maoEspadas[i].numero, maoEspadas[i].naipe);
+    }
+    fprintf(saida, "] index: %d\n", indexEspadas);
+
+
+    //Print Mão Paus
+    fprintf(saida, "Mao Paus: [ ");
+    for(i = 0; i < indexPaus; i++)
+    {
+    fprintf(saida, "%d%s ", maoPaus[i].numero, maoPaus[i].naipe);
+    }
+    fprintf(saida, "] index: %d\n", indexPaus);
+}
+
 int main() {
   char line[MAX_LINE];   // dados temporários
   char myId[ID_SIZE];    // identificador do bot
@@ -61,25 +183,31 @@ int main() {
 
 
   int i;
-  int indexCopas = 0, indexOuro = 0, indexPaus = 0, indexEspadas = 0;
+
 
   char valor;
-  char lixoBuffer = NULL;
+  char lixoBuffer;
   char naipeAtual[5];
   char *cartas;
-  Carta *mao;
+  
 
+  
+  Carta *mao;
   Carta *maoCopas;
   Carta *maoOuro;
   Carta *maoPaus;
   Carta *maoEspadas;
 
-  mao = malloc(sizeof(Carta) * 11);
+  Carta *lixo;
 
+  mao = malloc(sizeof(Carta) * 11);
   maoCopas = malloc(sizeof(Carta) * 11);
   maoOuro = malloc(sizeof(Carta) * 11);
-  maoPaus = malloc(sizeof(Carta) * 11);
   maoEspadas = malloc(sizeof(Carta) * 11);
+  maoPaus = malloc(sizeof(Carta) * 11);
+
+  lixo = malloc(sizeof(char) * 2);
+
 
   // DADOS DO INÍCIO DA PARTIDA
 
@@ -97,87 +225,24 @@ int main() {
   //
 
 
-  //Guardar as cartas iniciais aqui
+  //Guardar as cartas iniciais
   readline(line); 
   fprintf(saida, "Cartas iniciais: %s\n", line);
 
-  //Leitura das cartas e armazenamento
+  //Leitura das cartas separadamente e armazenamento
   cartas = strtok(line, " ");
   cartas = strtok(NULL, " ");
 
 
-
   //Recebendo os números da mão inicial | Separação da mão em naipes
     for (i = 0; i < 11; i++){
-    //Recebe o valor da carta
-    sscanf(cartas, "%c", &valor);
 
-    //Se o valor for o caractere 1 (ou seja, é o numero 10)
-    if (valor == '1')
-    {
-      //Recebe o valor (1), um lixoBuffer (0), e o naipeAtual
-      sscanf(cartas, "%c %c %s", &valor, &lixoBuffer, naipeAtual);
-    }
-    else{
-       //Recebe somente o valor e o naipeAtual
-       sscanf(cartas, "%c %s", &valor, naipeAtual);
-    }
-
-    //Colocando o valor recebido
-    if(valor == '1')
-    {
-      mao[i].numero = 10;
-    }
-    else if (valor == 'A')
-    {
-      mao[i].numero = 1;
-    }
-    else if(valor == 'J')
-    {
-      mao[i].numero = 11;
-    }
-    else if(valor == 'Q')
-    {
-      mao[i].numero = 12;
-    }
-    else if(valor == 'K')
-    {
-      mao[i].numero = 13;
-    }
-    else{
-      mao[i].numero = valor - '0';
-    }
-
-    //Colocando o naipe recebido na carta
-    strcpy(mao[i].naipe, naipeAtual);
+    //Colocando o valor da carta
+    valorCarta(cartas, &mao[i]);
 
     //Separação da mão em naipes
-    if((strcmp(naipeAtual, "♥") == 0))
-    {
-      strcpy(maoCopas[indexCopas].naipe, naipeAtual);
-      maoCopas[indexCopas].numero = mao[i].numero;
-      indexCopas++;
-    }
-    if((strcmp(naipeAtual, "♦") == 0))
-    {
-      strcpy(maoOuro[indexOuro].naipe, naipeAtual);
-      maoOuro[indexOuro].numero = mao[i].numero;
-      indexOuro++;
-    }
-    if((strcmp(naipeAtual, "♠") == 0))
-    {
-      strcpy(maoEspadas[indexEspadas].naipe, naipeAtual);
-      maoEspadas[indexEspadas].numero = mao[i].numero;
-      indexEspadas++;
-    }
-    if((strcmp(naipeAtual, "♣") == 0))
-    {
-      strcpy(maoPaus[indexPaus].naipe, naipeAtual);
-      maoPaus[indexPaus].numero = mao[i].numero;
-      indexPaus++;
-    }
+    tratamentoNaipe(&mao[i], maoCopas, maoOuro, maoEspadas, maoPaus);
     
-
     //Printa a carta atual
     fprintf(saida, "Carta[%d]: %d%s\n", i, mao[i].numero, mao[i].naipe);
 
@@ -186,8 +251,10 @@ int main() {
   
   //
   
+
   //Libera a mão inicial
   free(mao);
+
 
   //Ordenação das mãos iniciais de cada naipe
     bubblesortNaipes(maoCopas, indexCopas);
@@ -199,61 +266,82 @@ int main() {
 
 
   //Print da mao dividida em naipes
-    //Print Mão Copas
-    fprintf(saida, "Mao copas: [ ");
-    for (i = 0; i < indexCopas; i++){
-    fprintf(saida, "%d%s ", maoCopas[i].numero, maoCopas[i].naipe);
-    }
-    fprintf(saida, "]\n");
-    //Print Mão Ouro
-    fprintf(saida, "Mao ouro: [ ");
-    for (i = 0; i < indexOuro; i++)
-    {
-    fprintf(saida, "%d%s ", maoOuro[i].numero, maoOuro[i].naipe);
-    }
-    fprintf(saida, "]\n");
-    //Print Mão Espadas
-    fprintf(saida, "Mao espadas: [ ");
-    for(i = 0; i < indexEspadas; i++)
-    {
-    fprintf(saida, "%d%s ", maoEspadas[i].numero, maoEspadas[i].naipe);
-    }
-    fprintf(saida, "]\n");
-    //Print Mão Paus
-    fprintf(saida, "Mao Paus: [ ");
-    for(i = 0; i < indexPaus; i++)
-    {
-    fprintf(saida, "%d%s ", maoPaus[i].numero, maoPaus[i].naipe);
-    }
-    fprintf(saida, "]\n");
-  
+    printMaoNaipes(&mao[i], maoCopas, maoOuro, maoEspadas, maoPaus, saida);
   //
 
 
-  //Inicializar o lixo e ler a carta lida
-  readline(line);        // lê a carta inicial da pilha de descarte.
-
+  //Lê a carta inicial da pilha de descarte.
+  readline(line);  
   
+  fprintf(saida, "\n");
+  fprintf(saida, "\n");
+
+  //Definição da carta inicial do lixo
+    fprintf(saida,"lixo: %s\n", line); 
+
+    //Definição do lixo
+    valorCarta(line, &lixo[0]);
+  //
+  
+
+  //Print carta do lixo
+  fprintf(saida, "Carta do lixo: %d%s\n", lixo[0].numero, lixo[0].naipe);
+
+  fprintf(saida, "\n");
+  fprintf(saida, "\n");
+
   //Loop infinito gerenciado pelo juiz
   while(1) {              // DADOS DURANTE A PARTIDA
     do {                  // lê e descarta os dados até chegar sua vez
 
+      // sai do laço quando for enviado seu id
+      readline(line);     
 
-      readline(line);     // sai do laço quando for enviado seu id
-
-
-      fprintf(stderr, "%s\n", line);  // exemplo de saída para debugar
+      // exemplo de saída para debugar
+      //fprintf(saida, "Loop: %s\n", line);  
 
       //Verificar se o adversario deu o comando discard para atualizar o lixo
 
+
       //Verificar se o adversario deu o comando GET_DISCARD, neste caso é necessario zerar o lixo
+
 
     } while (strcmp(line, myId)); // sai do laço quando for a sua vez!
 
+    //Envia a ação para puxar uma carta
 
-    printf("GET_STOCK\n");         // envia a ação para puxar uma carta
-    readline(line);                // recebe a carta que puxou
-    printf("DISCARD %s\n", line);  // descarta a carta que puxou
+    //Tratamento de qual puxe quer fazer
+    char* acaoPuxar = puxar();
+    // fprintf(saida, "acao puxar: %s\n", acaoPuxar);
+    printf("%s\n", acaoPuxar);
+
+
+    //Recebe a carta que puxou
+    readline(line);
+    fprintf(saida, "Carta do deque de compra: %s\n", line);
+
+    //Recebimento da carta para a mãoo geral
+    valorCarta(line, &mao[0]);
+    fprintf(saida, "Carta recebida: %d%s\n", mao[0].numero, mao[0].naipe); 
+
+    //Separação da carta recebida para o naipe correspondente
+    tratamentoNaipe(&mao[0], maoCopas, maoOuro, maoEspadas, maoPaus);
+
+    //Ordenação das mãos de cada naipe
+      bubblesortNaipes(maoCopas, indexCopas);
+      bubblesortNaipes(maoOuro, indexOuro);
+      bubblesortNaipes(maoEspadas, indexEspadas);
+      bubblesortNaipes(maoPaus, indexPaus);
+    //
+
+    //Print das mãos
+    printMaoNaipes(&mao[i], maoCopas, maoOuro, maoEspadas, maoPaus, saida);
+
+    fprintf(saida, "\n");
+    fprintf(saida, "\n");
+
+    //Descarta a carta que puxou
+    printf("DISCARD %s\n", line);  
   }
 
   return 0;
